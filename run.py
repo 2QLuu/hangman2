4a3e694122feda82bff4841edf08cf9fcd9c3c03
2QLuu <lukasbittencourt.pro@gmail.com>
2023-04-16 09:25:12 +0000

    structure

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+word_list = ["python", "programming", "hangman", "game", "computer"]
+
+word = random.choice(word_list)
+
+character = input("guess: ")
+
+if character in word:
+    print("correct")
+else: print("incorrect")
