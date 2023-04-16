0371360233760ac34bb47c552d05396e999bf3d9
2QLuu <lukasbittencourt.pro@gmail.com>
2023-04-16 13:27:25 +0000

    fixed buggs that you couldnt enter letters in CAPS

--- hangman.py
+++ hangman.py
@@ -1,6 +1,24 @@
 import random
 
-word_list = ["python", "programming", "hangman", "game", "computer"]
+word_list = [
+    "python", "programming", "hangman", "game", "computer", "science",
+    "engineering", "mathematics", "physics", "chemistry", "biology",
+    "medicine", "history", "geography", "artificial", "intelligence",
+    "machine", "learning", "data", "analysis", "visualization",
+    "natural", "language", "processing", "network", "security",
+    "cybersecurity", "cryptography", "algorithms", "data", "structures",
+    "operating", "systems", "software", "engineering", "web", "development",
+    "mobile", "applications", "cloud", "computing", "internet", "of", "things",
+    "robotics", "automation", "virtual", "reality", "augmented", "reality",
+    "blockchain", "distributed", "systems", "cryptocurrencies", "quantum",
+    "computing", "nanotechnology", "biotechnology", "genetics", "climate",
+    "change", "sustainability", "renewable", "energy", "politics", "economics",
+    "finance", "marketing", "sales", "customer", "service", "leadership",
+    "management", "entrepreneurship", "innovation", "creativity", "design",
+    "user", "experience", "psychology", "philosophy", "religion", 
+    "spirituality"
+]
+
 
 word = random.choice(word_list)
 
@@ -11,12 +29,14 @@ unknown_word = []
 for i in range(0, len(word)):
     unknown_word.append('_')
 
-print(word)
-print(unknown_word)
 
 while True:
     print(unknown_word)
-    char = input("guess: ")
+    char = input("guess: ").lower()
+
+    if len(char) != 1:
+        print("Please enter only one letter!")
+        continue
 
     if char in word:
         print("correct")
